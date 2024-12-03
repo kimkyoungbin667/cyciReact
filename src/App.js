de917ed0ac7245a95936f6ec838dc0330e9ce084
kkb <rudqls1144@naver.com>
2024-12-03 14:17:05 +0900

    Feat: useState 사용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import Calc1 from './comp/calc/study01'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <About />
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/about"} element={<About />} />
+          <Route path={"/calc1"} element={<Calc1 />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -17,8 +21,7 @@ function App() {
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div style={{ border: '2px blue solid' }}>
       <Link to="/">Home으로 이동</Link>
     </div>
   )
@@ -29,6 +32,8 @@ function Home() {
     <div>
       <h1>Start Home</h1>
       <Link to="/about">About으로 이동</Link>
+      <br />
+      <Link to="/calc1">Cal1로 이동하기</Link>
     </div>
   )
 }
---
+++ src/comp/calc/study01.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+export default function Study() {
+
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [op, setOp] = useState('1');
+
+    const result = () => {
+        switch (op) {
+            case '1':
+                return Number(num1) + Number(num2);
+            case '2':
+                return Number(num1) - Number(num2);
+            case '3':
+                return Number(num1) * Number(num2);
+            case '4':
+                return Number(num1) / Number(num2);
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Study01</h1>
+            <input type='text' placeholder="첫 번째 수" value={num1}
+                onChange={e => { setNum1(e.target.value) }} />
+
+            <select onChange={e => { setOp(e.target.value) }}>
+                <option value="1">+</option>
+                <option value="2">-</option>
+                <option value="3">*</option>
+                <option value="4">/</option>
+            </select>
+
+            <input type='text' placeholder="두 번째 수" value={num2}
+                onChange={e => { setNum2(e.target.value) }} />
+
+            <h4>결과: {result()} </h4>
+        </div>
+    )
+
+}
