f2c01c88eeca8e41347db831e93bfe90caa6a6d2
kkb <rudqls1144@naver.com>
2024-12-04 09:45:03 +0900

    feat: axios get방식1

--- src/comp/ax/ax01.js
+++ src/comp/ax/ax01.js
@@ -1,7 +1,37 @@
+import axios from 'axios'
+import { useState } from 'react';
+
 export default function Ax1() {
+
+    const [지역, 변경지역] = useState([]);
+
+    function axios01() {
+        console.log('==== axios get 방식 ====');
+        //Promise와 비슷함
+        //ES 6문법
+        axios.get('http://localhost:8080/api/area/list')
+            .then(res => {
+                console.log(res);
+
+                //받은 데이터안에 코드가 200일떄(성공일떄)
+                if (res.data.code === 200) {
+                    //res.data.data ->지역 리스트
+                    변경지역(res.data.data);
+                }
+                
+
+            })
+
+
+
+
+
+    }
+
     return (
         <div>
             <h1>Axios 연습</h1>
+            <input type='button' value='get방식' onClick={axios01} />
         </div>
     )
 }
