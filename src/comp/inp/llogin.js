c49405d9b160683a5359b3d1c7441ace90906df1
kkb <rudqls1144@naver.com>
2024-12-05 10:29:24 +0900

    Feat: navigate 추가

--- src/comp/inp/llogin.js
+++ src/comp/inp/llogin.js
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Study() {
+
+    const navigate = useNavigate();
     return (
         <div>
             <h1>로그인</h1>
+            <input type ='text' placeholder="아이디 입력"/> <br/>
+            <input type='password' placeholder="패스워드 입력"/> <br/>
+
+            <input type='button' value='회원가입' onClick={() => navigate("/join")}/>
+            <input type='button' value='로그인'/>
+            
+            
         </div>
     )
 }
